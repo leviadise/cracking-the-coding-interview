8bf0879bef320ee435da35081127d13c04d672d8
leviadise <45793190+leviadise@users.noreply.github.com>
2021-01-07 13:25:29 +0200

    Create oneaway.py

---
+++ chapter1/oneaway.py
@@ -0,0 +1,50 @@
+from collections import Counter
+import unittest
+
+
+def one_away(word1, word2):
+    c = Counter(word1)
+    c += Counter(word2)
+    a = 0
+    for i in c:
+        if c[i] % 2 != 0:
+            if a != 0 :
+                if len(word1) != len(word2) or a != 1 :
+                    return False
+            a += 1
+    return True
+
+class Test(unittest.TestCase):
+    '''Test Cases'''
+    data = [
+        ('pale', 'ple', True),
+        ('pales', 'pale', True),
+        ('pale', 'bale', True),
+        ('paleabc', 'pleabc', True),
+        ('pale', 'ble', False),
+        ('a', 'b', True),
+        ('', 'd', True),
+        ('d', 'de', True),
+        ('pale', 'pale', True),
+        ('pale', 'ple', True),
+        ('ple', 'pale', True),
+        ('pale', 'bale', True),
+        ('pale', 'bake', False),
+        ('pale', 'pse', False),
+        ('ples', 'pales', True),
+        ('pale', 'pas', False),
+        ('pas', 'pale', False),
+        ('pale', 'pkle', True),
+        ('pkle', 'pable', False),
+        ('pal', 'palks', False),
+        ('palks', 'pal', False)
+    ]
+
+    def test_one_away(self):
+        for [test_s1, test_s2, expected] in self.data:
+            actual = one_away(test_s1, test_s2)
+            self.assertEqual(actual, expected)
+
+if __name__ == "__main__":
+    unittest.main()
+
