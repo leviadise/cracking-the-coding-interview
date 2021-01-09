e9e2637976aa55be31674398848ee188d586578f
leviadise <45793190+leviadise@users.noreply.github.com>
2021-01-09 20:43:10 +0200

    Update 16-17.py

--- chapter 16/16-17.py
+++ chapter 16/16-17.py
@@ -1,6 +1,7 @@
 def contiguous_sequence(numbers):
 	n= len(numbers)
 	ans = [0]*n
+	ans[n-1] = numbers[n-1]
 	for i in reversed(range(0,n-1)):
 		ans[i] = max(numbers[i] + ans[i+1] , numbers[i] )
 	return max(ans)
