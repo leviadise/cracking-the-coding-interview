8e58ce632e9ced8b9f80e0962c04cfaed56af196
leviadise <45793190+leviadise@users.noreply.github.com>
2021-01-09 20:42:26 +0200

    Create 16-17.py

---
+++ chapter 16/16-17.py
@@ -0,0 +1,9 @@
+def contiguous_sequence(numbers):
+	n= len(numbers)
+	ans = [0]*n
+	for i in reversed(range(0,n-1)):
+		ans[i] = max(numbers[i] + ans[i+1] , numbers[i] )
+	return max(ans)
+
+
+print(contiguous_sequence([2,-8,3,-2,4,-10]))
